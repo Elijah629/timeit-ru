71ed1e1c54b7461c4c0bda3851d36b4d47d0bf91
Elijah629 <62805599+Elijah629@users.noreply.github.com>
2023-05-29 17:05:47 -0700

    fix: show fractional part

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,5 @@ fn main() {
     unsafe { libc::system(command.as_ptr()) };
     let end_time = std::time::Instant::now();
     let diff = end_time - start_time;
-    print!("It took {}s", diff.as_secs());
+    print!("It took {}s", diff.as_secs_f64());
 }
