ff97c695b68423568bf1508569fd814680746d99
elijah629 <62805599+elijah629@users.noreply.github.com>
2023-06-26 10:26:05 -0700

    fix: use debug formatter fix: println! not print!

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ fn main() {
     let args: Vec<_> = std::env::args().collect();
     let program = &args[1..].join(" ");
     let command = std::ffi::CString::new(program.to_owned()).unwrap();
-    println!("Running: {}", program);
+
     let start_time = std::time::Instant::now();
     unsafe { libc::system(command.as_ptr()) };
     let end_time = std::time::Instant::now();
+
     let diff = end_time - start_time;
-    print!("It took {}s", diff.as_secs_f64());
+    println!("It took {:?}", diff);
 }
